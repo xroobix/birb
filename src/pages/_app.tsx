f3c16d91d328f42bc25399e9b910bb53782451a5
xroobix <109462535+xroobix@users.noreply.github.com>
2023-06-28 17:24:10 +0200

    Add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,20 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
-import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { api } from "~/utils/api";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ClerkProvider {...pageProps}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ClerkProvider>
   );
 };
 
