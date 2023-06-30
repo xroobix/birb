f6d5ba40dfb17f591f08e8000c505c18e58aeb1e
xroobix <109462535+xroobix@users.noreply.github.com>
2023-06-30 17:40:41 +0200

    Ignore build errors on vercel, swcMinify

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,17 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  swcMinify: true,
+
 };
 
 export default config;
