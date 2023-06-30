a2fcc1afc9ce465075f8750ce7ac6737f3ee8ec0
xroobix <109462535+xroobix@users.noreply.github.com>
2023-06-30 16:12:00 +0200

    Profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import "~/styles/globals.css";
 import { api } from "~/utils/api";
@@ -7,6 +8,11 @@ import { api } from "~/utils/api";
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Birb</title>
+        <meta name="description" content="🍗" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,5 @@
 import { clerkClient } from "@clerk/nextjs";
-import type { User } from "@clerk/nextjs/dist/types/server";
+
 import { TRPCError } from "@trpc/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
@@ -9,14 +9,9 @@ import {
   privateProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
+
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 
